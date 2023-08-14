58d980ba11d7a6d3e77af17a51f6e1390e9db48d
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-14 19:39:02 +0900

    [Gold V] Title: 감시 피하기, Time: 152 ms, Memory: 115980 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,69 +1,56 @@
 from itertools import combinations
-n=int(input())
-graph=[]
-for i in range(n):
-    graph.append(list(input().split()))
-#print(graph)
-xarr=[]
-tarr=[]
-sarr=[]
+
+
+def check_ok():
+    for sx,sy in teacher_lst:
+        for d in range(4):
+            x,y=sx,sy
+            for _ in range(n):
+                x=x+dx[d]
+                y=y+dy[d]
+                if (not in_range(x,y)) or arr[x][y]=="O":
+                    break
+                if arr[x][y]=="S":
+                    return False
+    return True
+
+
+
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+n=int(input())
+arr=[]
+for _ in range(n):
+    arr.append(input().split())
+
+teacher_lst=[]
+pos_lst=[]
 for i in range(n):
     for j in range(n):
-        if graph[i][j]=='X':
-            xarr.append((i,j))
-        elif graph[i][j]=='T':
-            tarr.append((i,j))
-        elif graph[i][j]=='S':
-            sarr.append((i,j))
+        if arr[i][j]=="T":
+            teacher_lst.append((i,j))
 
-# S발견하면 return False 발견 못하면 return True
-def t_check(r,c): 
-    for i in range(4):
-        temp_r=r
-        temp_c=c
-        for j in range(n):
-            temp_r+=dx[i]
-            temp_c+=dy[i]
-            #print(temp_r,temp_c)
-            if temp_r>=n or temp_r<0 or temp_c >=n or temp_c <0:
-                break
-            if graph[temp_r][temp_c]=='O':
-                break
-            if graph[temp_r][temp_c]=='S':
-                return False
-    return True
-             
-        
-#print(t_check(3,1))
+        elif arr[i][j]=="X":
+            pos_lst.append((i,j))
 
-comb_list=list(combinations(xarr,3))
-#print(comb_list)
 
-flag=0
-for a,b,c in comb_list:
-    #print(a,b,c)
-    find_s=0
-    graph[a[0]][a[1]]='O'
-    graph[b[0]][b[1]]='O'
-    graph[c[0]][c[1]]='O'
-    for x,y in tarr:
-        #print(x,y)
-        if t_check(x,y)==False:
-            find_s=1
-            break
-    if find_s==0:
-        flag=1
-        print("YES")
+result_flag=False
+case_lst=list(combinations(pos_lst,3))
+for case in case_lst:
+    for x,y in case:
+        arr[x][y]="O"
+
+    if check_ok():
+        result_flag=True
         break
-    
 
+    for x,y in case:
+        arr[x][y]="X"
 
-
-
-    graph[a[0]][a[1]]='X'
-    graph[b[0]][b[1]]='X'
-    graph[c[0]][c[1]]='X'
-if flag==0:
+if result_flag:
+    print("YES")
+else:
     print("NO")
