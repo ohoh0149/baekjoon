209a9eb1ca815551c15dd18b5869cd639a8dc34d
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-17 22:15:50 +0900

    [Gold IV] Title: 감시, Time: 412 ms, Memory: 124628 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.py
+++ 백준/Gold/15683. 감시/감시.py
@@ -1,77 +1,51 @@
 from itertools import product
-from copy import deepcopy
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<m
+
 n,m=map(int,input().split())
-graph=[]
-for i in range(n):
-    graph.append(list(map(int,input().split())))
+arr=[list(map(int,input().split())) for _ in range(n)]
 
-cctv=[]
-wall=[]
-dx=[0,1,0,-1]
-dy=[1,0,-1,0]
-def see(r,c,dir,graph):
-    nx=r
-    ny=c
-    for i in range(max(n,m)):
-        # print("nx,ny",nx,ny)
-        nx=nx+dx[dir]
-        ny=ny+dy[dir]
-        if nx>=n or nx<0 or ny>=m or ny <0 or graph[nx][ny]==6:
-            break
-        else:
-            graph[nx][ny]=9
-
-
-
+cctv_lst=[]
 for i in range(n):
     for j in range(m):
-        if 1<=graph[i][j]<=5:
-            cctv.append(([i,j,graph[i][j]]))
-        elif graph[i][j]==6:
-            wall.append([i,j])
-
-#print(graph)
-#print(cctv)
-#print(wall)
-def get_result(directions):#(1,1,2,3,4,1)
-    temp_graph=deepcopy(graph)
-    for i in range(len(cctv)):
-        n=cctv[i][2]
-        dir=directions[i]
-        #print("n",n)
-        if n==1:
-            see(cctv[i][0],cctv[i][1],dir,temp_graph)
-        elif n==2:
-            see(cctv[i][0],cctv[i][1],dir,temp_graph)
-            see(cctv[i][0],cctv[i][1],(dir+2)%4,temp_graph)
-        elif n==3:
-            see(cctv[i][0],cctv[i][1],dir,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-1)%4,temp_graph)
-        elif n==4:
-            see(cctv[i][0],cctv[i][1],dir,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-1)%4,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-2)%4,temp_graph)
-        elif n==5:
-            see(cctv[i][0],cctv[i][1],dir,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-1)%4,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-2)%4,temp_graph)
-            see(cctv[i][0],cctv[i][1],(4+dir-3)%4,temp_graph)
-    count=0
-    #print(temp_graph)
-    for i in temp_graph:
-        for j in i:
-            if j==0:
-                count+=1
-    return count
-
-
-dir_list=list(product([0,1,2,3],repeat=len(cctv)))
-#print(dir_list)
+        if 1<=arr[i][j]<=5:
+            cctv_lst.append((i,j))
+d_lst=[[0],[0],[0,2],[0,1],[0,1,2],[0,1,2,3]]
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 result=1e9
-for directions in dir_list:#(1,1,3,4...len(cctv))
-    temp_result=get_result(directions)
-    if result>temp_result:
-        result=temp_result
-
+case_lst=list(product([0,1,2,3] ,repeat=len(cctv_lst)))
+for case in case_lst:
+    visited=[[0]*m for _ in range(n)]
+    #case = [ 0, 3, 1 , 3 ,...cctv len]
+    for idx,d in enumerate(case):
+        ax,ay=cctv_lst[idx]
+        #print(ax,ay)
+        for cur_d in d_lst[arr[ax][ay]]:
+            x, y = ax,ay
+            visited[x][y]=1
+            rd=(cur_d+d)%4
+            for _ in range(max(n,m)):
+                x=x+dx[rd]
+                y=y+dy[rd]
+                if not in_range(x,y) or arr[x][y]==6:
+                    break
+                else:
+                    visited[x][y]=1
+    #print(visited)
+    count=0
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j]==0 and visited[i][j]==0:
+                count+=1
+    result=min(count,result)
 
 print(result)
+
+
+
+
+
+
+
