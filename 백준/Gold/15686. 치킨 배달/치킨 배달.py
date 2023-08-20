bc98a090121ec9191f0885be990a7fcc69c10169
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-20 19:50:40 +0900

    [Gold V] Title: 치킨 배달, Time: 244 ms, Memory: 118308 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,47 +1,45 @@
-from itertools import combinations
-import sys
-input=sys.stdin.readline
-def chk_sum(x,y):
-    sum=0
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            if graph[i][j]==1:
-                sum+=abs(i-x)+abs(j-y)
-    return sum
+def get_chi_len(x,y):
+    min_len=1e9
+    for cx,cy in cur_chi_lst:
+        min_len=min(min_len,abs(cx-x)+abs(cy-y))
+    return min_len
 
-def find_min_length(x,y,arr):
-    result=1e9
-    for i in arr:
-        if abs(i[0]-x)+abs(i[1]-y)<result:
-            result=abs(i[0]-x)+abs(i[1]-y)
-    return result
 
-n,m= map(int,input().split())
-graph=[[-1]*(n+1)]
-for i in range(n):
-    graph.append([-1]+list(map(int,input().split())))
-#print(graph)
+def get_result():
+    res=0
+    for hx,hy in house_lst:
+        res+=get_chi_len(hx,hy)
+    return res
 
-chickenlist=[]
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        if graph[i][j]==2:
-            chickenlist.append((i,j))
-# for i in range(1,m+1):
-#     temp=list(combinations(chickenlist,i))
-#     print(temp)
+
+def dfs(k,count):
+    global cur_chi_lst
+    global result
+    if count>m:
+        return
+    if k==l :
+        if count==m:
+            result=min(get_result(),result)
+        return
+    dfs(k+1,count)
+
+    cur_chi_lst.append(total_chi_lst[k])
+    dfs(k+1,count+1)
+    cur_chi_lst.pop()
 
 result=1e9
-for k in range(1,m+1):
-    comb=list(combinations(chickenlist,k))
-    for temp_chickenlist in comb:
-        sum=0
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if graph[i][j]==1:
-                    sum+=find_min_length(i,j,temp_chickenlist)
-        if sum<result:
-            result=sum
-            
+n,m=map(int,input().split())
+arr=[list(map(int,input().split())) for _ in range(n)]
+total_chi_lst=[]
+cur_chi_lst=[]
+house_lst=[]
+for i in range(n):
+    for j in range(n):
+        if arr[i][j]==1:
+            house_lst.append((i,j))
+        elif arr[i][j]==2:
+            total_chi_lst.append((i,j))
+
+l=len(total_chi_lst)
+dfs(0,0)
 print(result)
-        
