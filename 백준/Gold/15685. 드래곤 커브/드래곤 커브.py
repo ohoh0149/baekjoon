408e610352d37357fa24ac70b52cd720a347bf3c
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-20 19:28:51 +0900

    [Gold IV] Title: 드래곤 커브, Time: 156 ms, Memory: 115524 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -1,52 +1,52 @@
-import copy
-def turn_clock_dot(ax,ay,cx,cy):
-    rx=cx+cy-ay
-    ry=cy-cx+ax
-    return rx,ry
-
-def turn_clock_dot_arr(cx,cy,arr):
-    global temp_cx,temp_cy
-    ret_arr=copy.deepcopy(arr)
-    count=0
-    for x,y in arr:
-        rx,ry=turn_clock_dot(x,y,cx,cy)
-        if count==0:
-            temp_cx,temp_cy=rx,ry
-            count+=1
-        if (rx,ry) not in ret_arr:
-            
-            ret_arr.append((rx,ry))
-    
-    return ret_arr
-
-#turn_clock_dot_arr(4,3,[(5,1),(6,4)])
-
-
-
-graph=[[0]*101 for i in range(101)]
+def rotate_point(x,y,cx,cy):
+    return cy-y+cx,x-cx+cy
 
 dx=[1,0,-1,0]
 dy=[0,-1,0,1]
+def make_dragon_curve(x,y,d,g):
+    cx,cy=x+dx[d],y+dy[d]
+    pos_lst=[(x,y),(cx,cy)]
+    for _ in range(g):
+        for ax, ay in pos_lst[:]:
+            pos_lst.append(rotate_point(ax,ay,cx,cy))
+        cx,cy=rotate_point(x,y,cx,cy)
+    pos_lst = list(set(pos_lst))
+    return pos_lst
+    #print(pos_lst)
+
+def in_range(x,y):
+    return 0<=x<=100 and 0<=y<=100
+
+
+
 n=int(input())
-temp_cx=0
-temp_cy=0
-for i in range(n):
+
+#make_dragon_curve(0,0,0,3)
+#exit()
+arr=[[0]*101 for _ in range(101)]
+for _ in range(n):
     x,y,d,g=map(int,input().split())
-    temp_cx,temp_cy=x+dx[d],y+dy[d]
-    temp_dot_list=[(x,y),(temp_cx,temp_cy)]
-    for j in range(g):
-        #print("i,j,tempcx,tempcy",i,j,temp_cx,temp_cy)
-        temp_dot_list=turn_clock_dot_arr(temp_cx,temp_cy,temp_dot_list)
-    
-    for a,b in temp_dot_list:
-        graph[a][b]=1
+    pos_s=make_dragon_curve(x,y,d,g)
+    #print(pos_s)
+    for ax,ay in pos_s:
+        if in_range(ax,ay):
+            arr[ax][ay]=1
 
 
 result=0
-
 for i in range(100):
     for j in range(100):
-        if graph[i][j]==1 and graph[i+1][j]==1 and graph[i][j+1]==1 and graph[i+1][j+1]==1:
+        x,y=i,j
+        flag=True
+        for d in range(4):
+            if arr[x][y]==0:
+                flag=False
+                break
+            x,y=x+dx[(d+3)%4],y+dy[(d+3)%4]
+        if flag:
+            #print(i,j)
             result+=1
+
+
 print(result)
 
