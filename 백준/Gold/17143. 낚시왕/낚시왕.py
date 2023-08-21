8dd302a2c164d2a8f09435b05188a44243cf80be
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-21 21:32:35 +0900

    [Gold I] Title: 낚시왕, Time: 472 ms, Memory: 139024 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.py
+++ 백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,74 +1,70 @@
-def catch_shark(col):
-    global result
-    for i in range(1,r+1):
-        if arr[i][col]==0:
-            continue
-        else:
-            result+=arr[i][col][2]
-            arr[i][col]=0
-            break
+import copy
+
 
 def in_range(x,y):
-    if 1<=x<=r and 1<=y<=c:
-        return True
-    else:
-        return False
-
-def invert_d(d):
-    if d==1 or d==3:
-        d+=1
-    elif d==2 or d==4:
-        d-=1
-    else:
-        print("error!")
-    return d
-
-
-
+    return 0<=x<r and 0<=y<c
 dx=[0,-1,1,0,0]
 dy=[0,0,0,1,-1]
-def move_shark():
-    global arr
-    new_arr=[[0]*(c+1) for _ in range(r+1)]
-    for i in range(1,r+1):
-        for j in range(1,c+1):
-            #상어가 없으면 컨티뉴
-            if arr[i][j]==0:
-                continue
-            else:
-                #현재 위치
-                x,y=i,j
-                s=arr[i][j][0]
-                d= arr[i][j][1]
-                z=arr[i][j][2]
-                if d==1 or d==2:
-                    new_s=s%((r-1)*2)
-                else:
-                    new_s=s%((c-1)*2)
-                #속력만큼 반복하면서 이동해주기
-                for k in range(new_s):
-                    nx=x+dx[d]
-                    ny=y+dy[d]
-                    #범위 밖인 경우 좌표 변경 , 방향 반대
-                    if not in_range(nx,ny):
-                        d=invert_d(d)
-                        arr[i][j][1]=d
-                        nx=x+dx[d]
-                        ny=y+dy[d]
-                    x=nx
-                    y=ny
 
-                #최종 좌표(x,y)에 대해서 겹치는지 안겹치는 지 따져야 한다.
-                if new_arr[x][y]==0:
-                    new_arr[x][y]=[s,d,z]
-                #상어가 겹치는 경우 크기가 큰녀석이 이김
+
+
+def get_end_pos_sub(x,y,d):
+    if d==1:
+        return 0,y,x
+    elif d==2:
+        return r-1,y,r-1-x
+    elif d==3:
+        return x,c-1,c-1-y
+    elif d==4:
+        return x,0,y
+
+rd=[0,2,1,4,3]
+#좌표랑 방향 반환
+def get_next_pos(x,y,s,d):
+    if d==1 or d==2:
+        s=s%(2*(r-1))
+    else:
+        s=s%(2*(c-1))
+    #print(s)
+    nx=x+s*dx[d]
+    ny=y+s*dy[d]
+    if in_range(nx,ny):
+        return nx,ny,d
+    x,y,sub=get_end_pos_sub(x,y,d)
+    s-=sub
+    d=rd[d]
+
+    nx = x + s * dx[d]
+    ny = y + s * dy[d]
+    if in_range(nx, ny):
+        return nx,ny ,d
+
+    x, y, sub = get_end_pos_sub(x, y, d)
+    s -= sub
+    d = rd[d]
+
+    nx = x + s * dx[d]
+    ny = y + s * dy[d]
+    if in_range(nx, ny):
+        return nx,ny,d
+
+    print("this is error")
+    return -1,-1,-1
+
+
+
+def move_all_shark():
+    global arr
+    new_arr=[[0]*c for _ in range(r)]
+    for i in range(r):
+        for j in range(c):
+            if arr[i][j]!=0:
+                ni,nj,nd=get_next_pos(i,j,arr[i][j][0],arr[i][j][1])
+                if new_arr[ni][nj]==0:
+                    new_arr[ni][nj]=[arr[i][j][0],nd,arr[i][j][2]]
                 else:
-                    #원래 있던애가 큰경우
-                    if new_arr[x][y][2]>z:
-                        continue
-                    #현재 있던 애가 큰 경우 현재 녀석으로 교체
-                    else:
-                        new_arr[x][y]=[s,d,z]
+                    if new_arr[ni][nj][2]<arr[i][j][2]:
+                        new_arr[ni][nj]=[arr[i][j][0],nd,arr[i][j][2]]
     arr=new_arr
 
 
@@ -84,23 +80,31 @@ def move_shark():
 
 
 r,c,m=map(int,input().split())
-result=0
-arr=[[0]*(c+1) for _ in range(r+1)]
-for i in range(m):
-    x,y,s,d,z=map(int,input().split())
-    arr[x][y]=[s,d,z]
-#print(arr)
-def print_arr(arr):
-    for i in range(1,r+1):
-        print(arr[i][1:])
-#catch_shark(2)
-#print_arr(arr)
+arr=[[0]*c for _ in range(r)]
+for _ in range(m):
+    tr,tc,s,d,z=map(int,input().split())
+    arr[tr-1][tc-1]=[s,d,z]
+# for i in range(r):
+#     print(arr[i])
 #
-for j in range(1,c+1):
-    catch_shark(j)
-    #print("after catch",j)
-    #print_arr(arr)
-    move_shark()
-    #print("after move")
-    #print_arr(arr)
+# print(get_next_pos(0,2,5,2))
+#
+# print(get_next_pos(1,3,8,4))
+#
+# print(get_next_pos(3,0,3,3))
+#
+# print(get_next_pos(3,4,0,1))
+
+result=0
+for j in range(c):
+    for i in range(r):
+        if arr[i][j]!=0:
+            result+=arr[i][j][2]
+            #print(result)
+            arr[i][j]=0
+            break
+    move_all_shark()
 print(result)
+
+
+
