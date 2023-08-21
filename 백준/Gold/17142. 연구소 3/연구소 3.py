7f4adfd58d1406983e33f81cd42eabe51e854678
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-22 01:19:36 +0900

    [Gold III] Title: 연구소 3, Time: 248 ms, Memory: 117896 KB -BaekjoonHub

--- 백준/Gold/17142. 연구소 3/연구소 3.py
+++ 백준/Gold/17142. 연구소 3/연구소 3.py
@@ -1,77 +1,79 @@
-from itertools import combinations
 from collections import deque
-from copy import deepcopy
-n,m =map(int, input().split())
-graph=[]
-for i in range(n):
-    graph.append(list(map(int,input().split())))
-#print(graph)
-
-total_virus=[]
-for i in range(n):
-    for j in range(n):
-        if graph[i][j]==1:
-            graph[i][j]=-1
-        elif graph[i][j]==0:
-            graph[i][j]=-2
-        elif graph[i][j]==2:
-            graph[i][j]=-3
-            total_virus.append([i,j])
-#print(graph)
-comb_virus_list=list(combinations(total_virus,m))
-
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
-def check_nozero_max(arr):
-    max_value=0
-    for i in arr:
-        for j in i:
-            if j==-2:
-                return 0,max_value
-            max_value=max(max_value,j)
-            
-    return 1,max_value
-    
-min_count=1e9
-for virus_list in comb_virus_list:
-    #각 m개당
-    count=0
-    
-    #벽 -1 빈칸 -2 비활성 바이러스 -3
-    cur_graph=deepcopy(graph)
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def get_result():
     q=deque()
-    for i,j in virus_list:
-        q.append([i,j,0])
-        cur_graph[i][j]=0
+    visited=[[0]*n for _ in range(n)]
+    for vx,vy in choose_virus_lst:
+        q.append((vx,vy))
+        visited[vx][vy]=1
+
+    find_blank_count=0
+    res=-1
     while q:
-        x,y,temp_count=q.popleft()
-        for i in range(4):
-            nx=x+dx[i]
-            ny=y+dy[i]
-            if 0<=nx<n and 0<=ny<n and cur_graph[nx][ny]==-2:
-                cur_graph[nx][ny]=cur_graph[x][y]+1
-                q.append([nx,ny,temp_count+1])
-            elif 0<=nx<n and 0<=ny<n and cur_graph[nx][ny]==-3:
-                tc,maxxx=check_nozero_max(cur_graph)
-                if tc:
-                    
-                    min_count=min(min_count,maxxx)
-                    #print(cur_graph,maxxx,cur_graph[x][y])
-                cur_graph[nx][ny]=cur_graph[x][y]+1
-                q.append([nx,ny,temp_count+1])
-                
-    count=temp_count
-    check,maxx=check_nozero_max(cur_graph)
-    if check:
-        min_count=min(min_count,maxx)
-
-        
+        x,y=q.popleft()
+        if arr[x][y]==0:
+            find_blank_count+=1
+        if find_blank_count==blank_count:
+            res=visited[x][y]-1
+            break
+        for d in range(4):
+            nx=x+dx[d]
+            ny=y+dy[d]
+            if in_range(nx,ny) and visited[nx][ny]==0 and arr[nx][ny]!=1:
+                # if arr[nx][ny]==0:
+                #     find_blank_count+=1
+                visited[nx][ny]=visited[x][y]+1
+                q.append((nx,ny))
+    #print(visited)
+    return res
 
 
 
-if min_count==1e9:
-    min_count=-1
-print(min_count)
-    
-    
+
+
+
+
+
+def dfs(k,count):
+    global result
+    global choose_virus_lst
+    if count>m:
+        return
+    if k==l:
+        if count==m:
+            temp_result=get_result()
+            if temp_result!=-1:
+                result=min(result,temp_result)
+        return
+
+    dfs(k+1,count)
+    choose_virus_lst.append(total_virus_lst[k])
+    dfs(k+1,count+1)
+    choose_virus_lst.pop()
+
+
+n,m=map(int,input().split())
+arr=[list(map(int,input().split())) for _ in range(n)]
+total_virus_lst=[]
+
+blank_count=0
+for i in range(n):
+    for j in range(n):
+        if arr[i][j]==2:
+            total_virus_lst.append((i,j))
+        elif arr[i][j]==0:
+            blank_count+=1
+
+result=1e9
+l=len(total_virus_lst)
+choose_virus_lst=[]
+dfs(0,0)
+if result==1e9:
+    print(-1)
+else:
+    print(result)
 
