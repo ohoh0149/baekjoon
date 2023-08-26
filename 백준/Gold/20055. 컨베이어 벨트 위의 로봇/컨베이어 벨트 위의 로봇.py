1a0c745ca4a65bbecf94e9a0f6e3febacc1a0d1e
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-27 00:46:06 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 472 ms, Memory: 121156 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -17,12 +17,15 @@ def add_robot():
 def move_robot():
     global r_lst
     global A
+    global  zero_count
     for i in range(n - 2, -1, -1):
         if r_lst[i] == 0:
             continue
         elif r_lst[i] == 1:
             if A[i+1]>0 and r_lst[i+1]==0:
                 A[i+1]-=1
+                if A[i+1]==0:
+                    zero_count+=1
                 if i == n - 2:
                     r_lst[i] = 0
                 else:
@@ -30,6 +33,7 @@ def move_robot():
                     r_lst[i + 1] = 1
 
 
+zero_count=0
 n,k=map(int,input().split())
 lst=list(map(int,input().split()))
 
@@ -47,11 +51,9 @@ while True:
     if A[0]>0:
         r_lst[0]=1
         A[0]-=1
-    count=0
-    for a in A:
-        if a==0:
-            count+=1
-    if count>=k:
+        if A[0]==0:
+            zero_count+=1
+    if zero_count>=k:
         break
 print(result)
 
