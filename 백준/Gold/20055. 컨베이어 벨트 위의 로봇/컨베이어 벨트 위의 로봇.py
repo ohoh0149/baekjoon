7743f86200df0f54d421470048503c520f558d02
omh <69157614+ohoh0149@users.noreply.github.com>
2023-08-27 00:42:08 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 700 ms, Memory: 119876 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,46 +1,59 @@
 from collections import deque
 
-n,k= map(int,input().split())
-q=deque(map(int,input().split()))
 
-robot=deque()
+def add_robot():
+    global r_lst
+    for i in range(n - 2, -1, -1):
+        if r_lst[i] == 0:
+            continue
+        elif r_lst[i] == 1:
+            if i == n - 2:
+                r_lst[i] = 0
+            else:
+                r_lst[i] = 0
+                r_lst[i + 1] = 1
+
+
+def move_robot():
+    global r_lst
+    global A
+    for i in range(n - 2, -1, -1):
+        if r_lst[i] == 0:
+            continue
+        elif r_lst[i] == 1:
+            if A[i+1]>0 and r_lst[i+1]==0:
+                A[i+1]-=1
+                if i == n - 2:
+                    r_lst[i] = 0
+                else:
+                    r_lst[i] = 0
+                    r_lst[i + 1] = 1
+
+
+n,k=map(int,input().split())
+lst=list(map(int,input().split()))
+
+A=deque(lst)
+
+r_lst=[0]*n
+
+
 result=0
 while True:
     result+=1
-    #print(result)
-    #print(q)
-    # if result==5:
-    #     break
-
-    #1
-    q.appendleft(q.pop())
-    for i in range(len(robot)):
-        robot[i]+=1
-    if len(robot)>0 and robot[0]==n-1:
-        robot.popleft()
-    #2
-    before_robot=0
-    length=len(robot)
-    for i in range(length):
-        if q[robot[i]+1]>=1 and before_robot!=robot[i]+1:
-            robot[i]+=1
-            if robot[i]==n-1:
-                q[robot[i]]-=1
-                before_robot=0
-                continue
-            q[robot[i]]-=1
-        before_robot=robot[i]
-
-    if len(robot)>0 and robot[0]==n-1:
-        robot.popleft()
-    if q[0]>0:
-        robot.append(0)
-        q[0]-=1
-    if q.count(0)>=k:
+    A.appendleft(A.pop())
+    add_robot()
+    move_robot()
+    if A[0]>0:
+        r_lst[0]=1
+        A[0]-=1
+    count=0
+    for a in A:
+        if a==0:
+            count+=1
+    if count>=k:
         break
-    
 print(result)
-
 
 
 
