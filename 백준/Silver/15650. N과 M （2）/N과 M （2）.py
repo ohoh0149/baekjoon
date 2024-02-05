c55b9ef4edf0eab1db355aa8d492bf71d08c798b
omh <69157614+ohoh0149@users.noreply.github.com>
2024-02-06 06:14:24 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,18 @@
+n,m=map(int,input().split())
+
+
+lst=[]
+def dfs(k,count):
+    global lst
+    if k==n:
+        if count==m:
+            print(*lst)
+        return
+
+    lst.append(k+1)
+    dfs(k+1,count+1)
+    lst.pop()
+
+    dfs(k+1,count)
+
+dfs(0,0)
