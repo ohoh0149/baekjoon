04b18e0abbaeca0affaea6eeff22da743677d2f7
omh <69157614+ohoh0149@users.noreply.github.com>
2024-03-02 18:41:06 +0900

    [Gold V] Title: 수 고르기, Time: 196 ms, Memory: 114916 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -8,11 +8,11 @@ idx2=0
 max_val=1e11
 while idx2<n and idx1<n:
     a,b=lst[idx1],lst[idx2]
-    if b-a==0:
-        if m==0:
-            max_val=0
-        idx2+=1
-        continue
+    # if b-a==0:
+    #     if m==0:
+    #         max_val=0
+    #     idx2+=1
+    #     continue
     if b-a<m:
         idx2+=1
         continue
