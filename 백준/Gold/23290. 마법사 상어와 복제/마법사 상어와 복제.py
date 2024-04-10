0436bb7d7acf26df7622a9492f6830b04ba5a4fc
omh <69157614+ohoh0149@users.noreply.github.com>
2024-04-10 20:51:05 +0900

    [Gold I] Title: 마법사 상어와 복제, Time: 232 ms, Memory: 114144 KB -BaekjoonHub

--- 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
+++ 백준/Gold/23290. 마법사 상어와 복제/마법사 상어와 복제.py
@@ -1,125 +1,101 @@
 import copy
-def in_range(x,y):
-    return 1<=x<=4 and 1<=y<=4
-
-dx=[0,-1,-1,-1,0,1,1,1]
-dy=[-1,-1,0,1,1,1,0,-1]
-
-def move_fish():
-    global arr
-    new_arr=[[[0,0,0,0,0,0,0,0] for _ in range(5)] for _ in range(5)]
-    for i in range(1,5):
-        for j in range(1,5):
-            for l in range(8):
-                if arr[i][j][l]==0:
-                    continue
-                x,y=i,j
-                d=l
-                find=False
-                for _ in range(8):
-                    nx=x+dx[d]
-                    ny=y+dy[d]
-                    if in_range(nx,ny) and (not (sx==nx and sy==ny)) and smell_arr[nx][ny]<=0:
-                        new_arr[nx][ny][d]+=arr[x][y][l]
-                        find=True
-                        break
-                    d=(d+7)%8
-                if not find:
-                    new_arr[x][y][l]+=arr[x][y][l]
-    arr=new_arr
-
-def move_shark():
-    global arr
-    global smell_arr
-    global sx,sy
-    d_lst=[2,0,6,4]
-    max_count=-1
-    pos_lst=[]
-    for d1 in d_lst:
-        nx1=sx+dx[d1]
-        ny1=sy+dy[d1]
-        if not in_range(nx1,ny1):
-            continue
-        for d2 in d_lst:
-            nx2 = nx1 + dx[d2]
-            ny2 = ny1 + dy[d2]
-            if not in_range(nx2, ny2):
-                continue
-            for d3 in d_lst:
-                nx3 = nx2 + dx[d3]
-                ny3 = ny2 + dy[d3]
-                if not in_range(nx3, ny3):
-                    continue
-                #nx1 nx2 nx3
-
-                lst=list(set([(nx1,ny1),(nx2,ny2),(nx3,ny3)]))
-                count=0
-                for x,y in lst:
-                    count+=sum(arr[x][y])
-                if count>max_count:
-                    max_count=count
-                    pos_lst=[(nx1,ny1),(nx2,ny2),(nx3,ny3)]
-
-
-    sx,sy=pos_lst[2]
-
-    for x,y in pos_lst:
-        if sum(arr[x][y])!=0:
-            arr[x][y]=[0,0,0,0,0,0,0,0]
-            smell_arr[x][y]=3
-
-def down_smell():
-    global smell_arr
-    for i in range(1,5):
-        for j in range(1,5):
-            if smell_arr[i][j]>=1:
-                smell_arr[i][j]-=1
-
-def print_arr(arr):
-    for i in range(1,5):
-        print(arr[i][1:])
-    print()
 
 m,s=map(int,input().split())
-
-smell_arr=[[0]*5 for _ in range(5)]
-
-arr=[[[0,0,0,0,0,0,0,0] for _ in range(5)] for _ in range(5)]
+arr=[[[0]*8 for _ in range(4)] for _ in range(4)]
 for _ in range(m):
-    fx,fy,fd=map(int,input().split())
-    arr[fx][fy][fd-1]+=1
+    x,y,d=map(int,input().split())
+    arr[x-1][y-1][d-1]+=1
 sx,sy=map(int,input().split())
+sx-=1
+sy-=1
+
+smell_arr=[[0]*4 for _ in range(4)]
 
 
+f_dx=[0,-1,-1,-1,0,1,1,1]
+f_dy=[-1,-1,0,1,1,1,0,-1]
+s_dx=[-1,0,1,0]
+s_dy=[0,-1,0,1]
+
+def in_range(x,y):
+    return 0<=x<4 and 0<=y<4
+def move_fish(arr):
+    new_arr=[[[0]*8 for _ in range(4)] for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            for sd in range(8):
+                if arr[i][j][sd]==0:
+                    continue
+                flag=False
+                for t in range(8):
+                    d=(sd-t+8)%8
+                    nx=i+f_dx[d]
+                    ny=j+f_dy[d]
+                    if in_range(nx,ny) and (nx,ny)!=(sx,sy) and smell_arr[nx][ny]==0:
+                        new_arr[nx][ny][d]+=arr[i][j][sd]
+                        flag=True
+                        break
+                if not flag:
+                    new_arr[i][j][sd]+=arr[i][j][sd]
+
+    return new_arr
 
 
 for _ in range(s):
-
-
-
+    #1
     copy_arr=copy.deepcopy(arr)
-    move_fish()
 
+    #2
+    arr=move_fish(arr)
 
+    #3
+    result_d_lst=[]
+    max_count=0
+    for d1 in range(4):
+        for d2 in range(4):
+            for d3 in range(4):
+                d_lst=[d1,d2,d3]
+                cur_count=0
+                out_flag=False
+                cx,cy=sx,sy
+                visit_set=set()
+                for d in d_lst:
+                    cx=cx+s_dx[d]
+                    cy=cy+s_dy[d]
+                    if not in_range(cx,cy):
+                        out_flag=True
+                        break
+                    if (cx,cy) not in visit_set:
+                        cur_count+=sum(arr[cx][cy])
+                        visit_set.add((cx,cy))
+                if not out_flag:
+                    if not result_d_lst:
+                        result_d_lst=[d1,d2,d3]
+                    if max_count<cur_count:
+                        max_count=cur_count
+                        result_d_lst=[d1,d2,d3]
 
-    move_shark()
+    for d in result_d_lst:
+        sx=sx+s_dx[d]
+        sy=sy+s_dy[d]
+        if sum(arr[sx][sy])>0:
+            smell_arr[sx][sy]=3
+            for t in range(8):
+                arr[sx][sy][t]=0
+    #4
+    for i in range(4):
+        for j in range(4):
+            if smell_arr[i][j]>0:
+                smell_arr[i][j]-=1
 
-
-    down_smell()
-
-    for i in range(1,5):
-        for j in range(1,5):
-            for l in range(8):
-                arr[i][j][l]+=copy_arr[i][j][l]
-
-
-
-
-
+    for i in range(4):
+        for j in range(4):
+            for d in range(8):
+                arr[i][j][d]+=copy_arr[i][j][d]
 
 
 result=0
-for i in range(1,5):
-    for j in range(1,5):
+for i in range(4):
+    for j in range(4):
         result+=sum(arr[i][j])
 print(result)
