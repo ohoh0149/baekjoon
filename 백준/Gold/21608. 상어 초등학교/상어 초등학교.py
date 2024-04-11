8f3d9666a01f95030ebf085cbcf0b360f515e4c3
omh <69157614+ohoh0149@users.noreply.github.com>
2024-04-11 18:55:55 +0900

    [Gold V] Title: 상어 초등학교, Time: 208 ms, Memory: 112460 KB -BaekjoonHub

--- 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
+++ 백준/Gold/21608. 상어 초등학교/상어 초등학교.py
@@ -1,110 +1,56 @@
 n=int(input())
-arr=[]
-for i in range(n*n):
-    arr.append(list(map(int,input().split())))
-###print(arr)
-
-classroom=[[0]*n for i in range(n)]
-
 
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
-def get_like_count(x,y,student_num,like_arr):
-    ##print("like_arr",like_arr)
-    count=0
-    ##print("x y ",x,y)
-    for i in range(4):
-        nx=x+dx[i]
-        ny=y+dy[i]
-        ##print("classroom",classroom)
-        ##print("nx ny ",nx,ny)
-        if 0<=nx<n and 0<=ny<n and classroom[nx][ny] in like_arr:
-            count+=1
-    return count
-
-
-def get_max_like_index(num_like_arr):#[student_num, like1,like2,lik3,lik4]
-    temp_arr=[[0]*n for i in range(n)]
-    return_arr=[]
-    student_num=num_like_arr[0]
-    like_arr=num_like_arr[1:5]
-    max_count=0
-    zeroCount=0
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+def find_pos(num):
+    max_val=(-1,-1)
+    rx,ry=-1,-1
     for i in range(n):
         for j in range(n):
-            if classroom[i][j]!=0:
+            if arr[i][j]!=0:
                 continue
-            ##print("i,j",i,j)
-            zeroCount+=1
-            temp_arr[i][j]=get_like_count(i,j,student_num,like_arr)
+            like_count=0
+            blank_count=0
+            for d in range(4):
+                nx=i+dx[d]
+                ny=j+dy[d]
+                if in_range(nx,ny):
+                    if arr[nx][ny]==0:
+                        blank_count+=1
+                    else:
+                        if arr[nx][ny] in dic[num]:
+                            like_count+=1
+            if max_val<(like_count,blank_count):
+                max_val=(like_count,blank_count)
+                rx,ry=i,j
+    return rx,ry
 
-            if max_count<=temp_arr[i][j]:
-                max_count=temp_arr[i][j]
-    if max_count==0:
-        for i in range(n):
-            for j in range(n):
-                if classroom[i][j]==0:
-                    return_arr.append((i,j))
-        return return_arr
-    for i in range(n):
-        for j in range(n):
-            if temp_arr[i][j]==max_count:
-                return_arr.append((i,j))
-    ##print(max_count)
-    ##print("temp_arr",temp_arr)
-    return return_arr
+arr=[[0]*n for _ in range(n)]
+num_lst=[]
+dic=dict()
+for _ in range(n*n):
+    lst=list(map(int,input().split()))
+    num=lst.pop(0)
+    num_lst.append(num)
+    dic[num]=set(lst)
 
-def count_empty(r,c):
-    count=0
-    for i in range(4):
-        nx=r+dx[i]
-        ny=c+dy[i]
-        if 0<=nx<n and 0<=ny<n and classroom[nx][ny] ==0:
-            count+=1
-    ###print(r,c,count)
-    return count
+for num in num_lst:
+    x,y=find_pos(num)
+    arr[x][y]=num
 
-for num_like_arr in arr:
-    ##print("num_like_arr",num_like_arr)
-    candidate=get_max_like_index(num_like_arr)
-    #print("candidate",candidate)
-    if len(candidate)==1:
-        classroom[candidate[0][0]][candidate[0][1]]=num_like_arr[0]
-      #  print(classroom)
-        continue
-    if len(candidate)==0:
-        continue
-    max_count=0
-    rx=candidate[0][0]
-    ry=candidate[0][1]
-    for r,c in candidate:
-        if count_empty(r,c)>max_count and classroom[r][c]==0:
-            max_count=count_empty(r,c)
-            rx=r
-            ry=c
-            ##print("rx,ry",rx,ry)
-    classroom[rx][ry]=num_like_arr[0]
-    #print(classroom)
-
-temp_index_arr=[None]*(n*n+1)
+temp_lst=[0,1,10,100,1000]
 result=0
-for num_like_arr in arr:
-    temp_index_arr[num_like_arr[0]]=(num_like_arr[1:5])
-#print("temp_index_arr",temp_index_arr)
-
 for i in range(n):
     for j in range(n):
-        temp_count=0
-        if classroom[i][j]!=0:
-            temp_count+=get_like_count(i,j,classroom[i][j],temp_index_arr[classroom[i][j]])
-        if temp_count==0:
-            continue
-        result+=(10**(temp_count-1))
+        count=0
+        for d in range(4):
+            nx=i+dx[d]
+            ny=j+dy[d]
+            if in_range(nx,ny) and arr[nx][ny] in dic[arr[i][j]]:
+                count+=1
+        result+=temp_lst[count]
 
-    
-#print(classroom)
 print(result)
-
-    
-
 
